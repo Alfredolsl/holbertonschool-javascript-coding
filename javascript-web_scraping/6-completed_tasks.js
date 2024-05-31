68a45153be59a31f9e2af50bd89416850c6d8358
root <alfredo.445566@gmail.com>
2024-05-31 03:50:41 +0000

    add script that computes number of tasks completed by user id

---
+++ javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// script that computes the number of tasks completed by user id
+// only print users with completed tasks
+const request = require('request');
+const url = process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching the URL:', error);
+    return;
+  }
+  const tasksDoneByUsers = {};
+  const json = JSON.parse(body);
+
+  for (let i = 0; i < json.length; i++) {
+    const userID = json[i].userId;
+    const completed = json[i].completed;
+
+    // check if user is not in tasksDoneByUsers to add user
+    if (!(userID in tasksDoneByUsers)) {
+      tasksDoneByUsers[userID] = 0;
+    }
+    // check if task is completed
+    if (completed) {
+      ++tasksDoneByUsers[userID];
+    }
+  }
+  console.log(tasksDoneByUsers);
+});
