723ebd26d60a3248d94aad1e96a6f988682cec5c
root <alfredo.445566@gmail.com>
2024-05-31 03:56:41 +0000

    semistandard changes

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // script that reads and prints the content of a file
 const fs = require('fs');
-file_path = process.argv[2]
+const filePath = process.argv[2];
 
-fs.readFile(file_path, 'utf-8', function (err,data) {
+fs.readFile(filePath, 'utf-8', function (err, data) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
     console.log(data);
   }
