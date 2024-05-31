3c3f2ffd4d02e2cc5cad4d1a61e8366f2f0b8024
root <alfredo.445566@gmail.com>
2024-05-31 01:03:12 +0000

    semistandard changes

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,6 +3,10 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 const id = process.argv[2];
 
-request({url: `${url}${id}`, json: true}, (error, response, body) =>
-  console.log(body['title'])
-);
+request({ url: `${url}${id}`, json: true }, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching the URL:', error);
+    return;
+  }
+  console.log(body.title);
+});
