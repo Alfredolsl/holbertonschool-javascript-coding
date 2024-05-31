83024b47faad0b2c30d0fa2473461f8b6d33171f
root <alfredo.445566@gmail.com>
2024-05-31 02:27:38 +0000

    add for loop to iterate and confirm character apparition

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = `${process.argv[2]}`;
 
 request({ url: `${url}`, json: true }, (error, response, body) => {
   if (error) {
     console.error('Error fetching the URL:', error);
     return;
   }
-  console.log((body.films).length);
+  results = body.results;
+  let appearances = 0;
+  for (let i = 0; i < results.length; i++) {
+    const characters = results[i].characters;
+    for (let j = 0; j < characters.length; j++) {
+      if (characters[j].includes('18')){
+	appearances++;
+      }
+    }
+  }
+  console.log(appearances);
 });
