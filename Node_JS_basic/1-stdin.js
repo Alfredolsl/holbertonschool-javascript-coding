acca6873d5df8f135308271a24ac8e4abe25fc4d
root <alfredo.445566@gmail.com>
2024-06-01 19:05:33 +0000

    suppress output to avoid displaying the input

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,10 +2,12 @@ const readLine = require('readline');
 
 const rl = readLine.createInterface({
   input: process.stdin,
-  output: process.stdout,
+  output: null,
 });
 
-rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.question('', (name) => {
   console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
   rl.close();
