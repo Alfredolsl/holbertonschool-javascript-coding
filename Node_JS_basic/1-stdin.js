c4b0bdf3ef1e1ac11be5e443a0c35249aa477ff6
root <alfredo.445566@gmail.com>
2024-06-02 01:21:43 +0000

    modify script to use process api

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-const readLine = require('readline');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: null,
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
