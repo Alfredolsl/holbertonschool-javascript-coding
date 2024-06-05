7f5b2e4f356e251496b2491483f48d2d931618fc
root <alfredo.445566@gmail.com>
2024-06-05 21:40:25 +0000

    revamp StudentsController

--- Node_JS_basic/full_server/controllers/StudentsController.js
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,18 +5,21 @@ class StudentsController {
     readDatabase(DATABASE)
       .then((fields) => {
         const students = [];
-        let message;
-      
+
+        let msg;
+
         students.push('This is the list of our students');
 
         for (const key of Object.keys(fields)) {
-          message = `Number of students in ${key}: ${fields[key].length}. List: ${fields[key].join(', ')}`
-	  students.push(message);
-	}
+          msg = `Number of students in ${key}: ${fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          students.push(msg);
+        }
         response.send(200, `${students.join('\n')}`);
       })
       .catch(() => {
-        response.send(500, 'Cannot load database');
+        response.send(500, 'Cannot load the database');
       });
   }
 
@@ -24,14 +27,14 @@ class StudentsController {
     const { major } = request.params;
 
     if (major !== 'CS' && major !== 'SWE') {
-      response.send(500, 'Major parameter should be CS or SWE');
+      response.send(500, 'Major parameter must be CS or SWE');
     } else {
-      readDataBase(DATABASE)
+      readDatabase(DATABASE)
         .then((fields) => {
-	  const students = fields[major];
+          const students = fields[major];
 
           response.send(200, `List: ${students.join(', ')}`);
-	})
+        })
         .catch(() => response.send(500, 'Cannot load the database'));
     }
   }
